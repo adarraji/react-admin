336585df6e373a36eba13287fe8bde63e8236107
Ali Darraji <techbrain203@outlook.com>
2022-09-16 15:36:23 -0400

    created routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import List from './pages/List';
+import Single from './pages/Single';
+import NewPage from './pages/NewPage';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="users">
+            <Route index element={<List />} />
+            <Route path=":userId" element={<Single />} />
+            <Route path="new" element={<NewPage />} />
+          </Route>
+        </Route>
       </Routes>
     </Router>
   );
