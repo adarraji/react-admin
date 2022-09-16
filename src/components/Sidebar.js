7895ac7ef1b76a27166d79bb1c3f5f800af54049
Ali Darraji <techbrain203@outlook.com>
2022-09-16 15:45:02 -0400

    basic sidebar sections

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,30 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
 
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <Container>
+            <Top></Top>
+            <Center></Center>
+            <Bottom></Bottom>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    
+`;
+
+const Top = styled.div`
+    
+`;
+
+const Center = styled.div`
+    
+`;
+
+const Bottom = styled.div`
+    
+`;
 
 export default Sidebar
