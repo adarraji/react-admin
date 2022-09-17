520b3a3b294d42de694b6a687eb6ef3eb1c0138c
Ali Darraji <techbrain203@outlook.com>
2022-09-17 14:07:43 -0400

    created featured and chart component

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,20 +3,25 @@ import styled from "styled-components";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import Widget from "../components/Widget";
+import Featured from "../components/Featured";
+import Chart from "../components/Chart";
+
 
 const home = () => {
     return (
         <Container>
             <Sidebar />
-            <HomeContainer>
-                <Navbar />
-                <Widgets>
-                    <Widget type="user" />
-                    <Widget type="order" />
-                    <Widget type="earning" />
-                    <Widget type="balance" />
-                </Widgets>
-            </HomeContainer>
+            <Navbar />
+            <Widgets>
+                <Widget type="user" />
+                <Widget type="order" />
+                <Widget type="earning" />
+                <Widget type="balance" />
+            </Widgets>
+            <Charts>
+                <Featured />
+                <Chart />
+            </Charts>
         </Container>
     )
 }
@@ -33,6 +38,9 @@ const Widgets = styled.div`
    display: flex;
    padding: 20px;
    gap: 20px;
+`;
+
+const Charts = styled.div`
 `;
 
 
