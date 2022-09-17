c8ba4dffdce174b78c755f2652151b27efddda19
Ali Darraji <techbrain203@outlook.com>
2022-09-17 14:19:57 -0400

    charts and featured  styles

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,9 +1,18 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
+
 
 const Chart = () => {
   return (
-    <div>Chart</div>
+    <Container>Chart</Container>
   )
 }
+
+const Container = styled.div`
+   flex: 4;
+   -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+    box-shadow: 2px 4px 10px 1px rgba(201, 201, 201, 0.47);
+    padding: 10px;
+`;
 
 export default Chart
--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,9 +1,17 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
+
 
 const Featured = () => {
     return (
-        <div>Featured</div>
+        <Container>Featured</Container>
     )
 }
+
+const Container = styled.div`
+   flex: 2;
+   -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+    box-shadow: 2px 4px 10px 1px rgba(201, 201, 201, 0.47);
+`;
 
 export default Featured
