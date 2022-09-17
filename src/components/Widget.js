b9f1ca44f52bf59f284a4130b5d2b629a42debec
Ali Darraji <techbrain203@outlook.com>
2022-09-17 10:57:17 -0400

    switch/case block for type

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -2,8 +2,72 @@ import React from "react";
 import styled from "styled-components";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+
+
 
 const Widget = ({ type }) => {
+
+    let data;
+
+    switch (type) {
+        case "user":
+            data = {
+                title: "USERS",
+                isMoney: false,
+                link: "See all users",
+                icon: (
+                    <Icon>
+                        <PersonOutlinedIcon style={{ color: "crimson", backgroundColor: "rgba(255, 0, 0, 0.2)" }} />
+                    </Icon>
+                )
+            };
+            break;
+
+        case "order":
+            data = {
+                title: "ORDERS",
+                isMoney: false,
+                link: "View all orders",
+                icon: (
+                    <Icon>
+                        <ShoppingCartOutlinedIcon style={{ color: "goldenrod", backgroundColor: "rgba(218, 165, 32, 0.2)" }} />
+                    </Icon>
+                )
+            };
+            break;
+
+        case "earning":
+            data = {
+                title: "EARNINGS",
+                isMoney: true,
+                link: "View net earnings",
+                icon: (
+                    <Icon>
+                        <MonetizationOnOutlinedIcon style={{ color: "green", backgroundColor: "rgba(0, 128, 0, 0.2)" }} />
+                    </Icon>
+                )
+            };
+            break;
+
+        case "balance":
+            data = {
+                title: "BALANCE",
+                isMoney: true,
+                link: "See details",
+                icon: (
+                    <Icon>
+                        <MonetizationOnOutlinedIcon style={{ color: "purple", backgroundColor: "rgba(128, 0, 128, 0.2)" }} />
+                    </Icon>
+                )
+            };
+            break;
+
+        default:
+            break;
+    };
+
     return (
         <Container>
             <Left>
