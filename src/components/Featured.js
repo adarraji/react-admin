1f4e9df8d338fdb2c22b83f533de782ede4c59d2
Ali Darraji <techbrain203@outlook.com>
2022-09-17 14:27:32 -0400

    added basic sections

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,10 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
 
 
 const Featured = () => {
     return (
-        <Container>Featured</Container>
+        <Container>
+            <Top>
+                <Title>Total Revenue</Title>
+                <MoreVertIcon fontSize="small" />
+            </Top>
+            <Bottom>
+                <FeaturedChart>
+
+                </FeaturedChart>
+            </Bottom>
+        </Container>
     )
 }
 
@@ -13,5 +25,36 @@ const Container = styled.div`
    -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
     box-shadow: 2px 4px 10px 1px rgba(201, 201, 201, 0.47);
 `;
+
+const Top = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: gray;
+`;
+
+const Title = styled.h1`
+    font-size: 16px;
+    font-weight: 500;
+`;
+
+const Bottom = styled.div`
+
+`;
+
+const FeaturedChart = styled.div`
+
+`;
+
+// const Bottom = styled.div`
+
+// `;
+// const Bottom = styled.div`
+
+// `;
+
+// const Bottom = styled.div`
+
+// `;
 
 export default Featured
