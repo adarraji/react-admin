04c9fdd2c87e28974441eee44c869e2ad3a73100
Ali Darraji <techbrain203@outlook.com>
2022-09-17 15:26:13 -0400

    adding components

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -4,7 +4,9 @@ import styled from "styled-components";
 
 const Chart = () => {
   return (
-    <Container>Chart</Container>
+    <Container>
+      <Title>{"Charts"}</Title>
+    </Container>
   )
 }
 
@@ -13,6 +15,15 @@ const Container = styled.div`
    -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
     box-shadow: 2px 4px 10px 1px rgba(201, 201, 201, 0.47);
     padding: 10px;
+    color: gray;
+`;
+
+const Title = styled.div`
+  margin-top: 20px;
+`;
+
+const ChartGrid = styled.div`
+  stroke: rgb(228, 225, 225);
 `;
 
 export default Chart
