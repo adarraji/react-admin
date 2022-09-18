4e0ca122a4b52762dec9ce4c83888b12399739ff
Ali Darraji <techbrain203@outlook.com>
2022-09-18 12:21:56 -0400

    create datatable components

---
+++ src/components/DataTable.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom"
+
+
+const DataTable = () => {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    
+`;
+
+const Title = styled.div`
+    
+`;
+
+const LinkTo = styled.div`
+    
+`;
+
+export default DataTable
