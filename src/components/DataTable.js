c24b1e4f72e70c09f6d2b323ec506a6df1bc7ac8
Ali Darraji <techbrain203@outlook.com>
2022-09-18 12:58:11 -0400

    fix

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { userColumns, userRows } from "../../datatablesource";
+import { userColumns, userRows } from "../datatablesource";
 import { Link } from "react-router-dom";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 
