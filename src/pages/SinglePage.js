507e76fc949cc546be1bd05f06de4f88c76de397
adarraji <a.darraji@outlook.com>
2022-09-19 10:52:22 -0400

    added components

--- src/pages/SinglePage.js
+++ src/pages/SinglePage.js
@@ -1,9 +1,30 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+import Chart from "../components/Chart";
+import List from "../components/Table";
 
 const SinglePage = () => {
     return (
-        <div>single</div>
+        <Container>
+            <Sidebar />
+            <SingleContainer>
+                <Navbar />
+                Single
+            </SingleContainer>
+        </Container>
     )
 }
+
+const Container = styled.div`
+   display: flex;
+   width: 100%;
+`;
+
+const SingleContainer = styled.div`
+   flex:6;
+`;
+
 
 export default SinglePage
