d61a04bc34078070549d694128c46e277957d8f6
Ali Darraji <techbrain203@outlook.com>
2022-09-19 19:46:14 -0400

    added information section

--- src/pages/SinglePage.js
+++ src/pages/SinglePage.js
@@ -13,13 +13,32 @@ const SinglePage = () => {
                 <Navbar />
                 <Top>
                     <Left>
+                        <EditButton>Edit</EditButton>
                         <Title>Information</Title>
                         <Item>
                             <ItemImage
                                 src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
                                 alt=""
                             />
-                            <Details>Details</Details>
+                            <Details>
+                                <ItemTitle>Jane Doe</ItemTitle>
+                                <DetailItem>
+                                    <ItemKey>Email:</ItemKey>
+                                    <ItemValue>janedoe@gmail.com</ItemValue>
+                                </DetailItem>
+                                <DetailItem>
+                                    <ItemKey>Phone:</ItemKey>
+                                    <ItemValue>+1 2345 6789</ItemValue>
+                                </DetailItem>
+                                <DetailItem>
+                                    <ItemKey>Address:</ItemKey>
+                                    <ItemValue>Elton St. 234 Garden Yd. NewYork</ItemValue>
+                                </DetailItem>
+                                <DetailItem>
+                                    <ItemKey>Country:</ItemKey>
+                                    <ItemValue>USA</ItemValue>
+                                </DetailItem>
+                            </Details>
                         </Item>
                     </Left>
                     <Right>Right</Right>
@@ -52,10 +71,14 @@ const Left = styled.div`
     -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
     box-shadow: 2px 4px 10px 1px rgba(201, 201, 201, 0.47);
     padding: 20px;
+    position: relative;
 `;
 
 const Title = styled.h1`
-   
+   font-size: 16px;
+   color: lightgray;
+   margin-bottom: 20px;
+
 `;
 
 const Item = styled.div`
@@ -72,6 +95,38 @@ const ItemImage = styled.img`
 
 const Details = styled.div`
    
+`;
+
+const ItemTitle = styled.h1`
+   margin-bottom: 10px;
+   color: #555;
+`;
+
+const DetailItem = styled.div`
+   margin-bottom: 10px;
+   font-size: 14px;
+`;
+
+const ItemKey = styled.span`
+   font-weight: bold;
+   color: gray;
+   margin-right: 5px;
+`;
+
+const ItemValue = styled.span`
+   font-weight: 300;
+`;
+
+const EditButton = styled.div`
+   position: absolute;
+   right: 0;
+   top: 0;
+   padding: 5px;
+    font-size: 12px;
+    color: #7451f8;
+    background-color: #7551f818;
+    cursor: pointer;
+    border-radius: 0px 0px 0px 5px;
 `;
 
 
