81583d616e467f7fd44c803b7ff0d4a204fc7db6
adarraji <a.darraji@outlook.com>
2022-09-20 09:52:29 -0400

    added components and sections

--- src/pages/NewPage.js
+++ src/pages/NewPage.js
@@ -11,11 +11,44 @@ const NewPage = () => {
             <NewContainer>
                 <Navbar />
                 <Top>
-
+                    <Title>Add New User</Title>
                 </Top>
                 <Bottom>
-                    <Left></Left>
-                    <Right></Right>
+                    <Left>
+                        <Image alt="" src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" />
+                    </Left>
+                    <Right>
+                        <Form>
+                            <FormInput>
+                                <Label>Username</Label>
+                                <Input type="text" placeholder="john_doe" />
+                            </FormInput>
+                            <FormInput>
+                                <Label>Full Name</Label>
+                                <Input type="text" placeholder="John Doe" />
+                            </FormInput>
+                            <FormInput>
+                                <Label>Email</Label>
+                                <Input type="email" placeholder="john_doe@gmail.com" />
+                            </FormInput>
+                            <FormInput>
+                                <Label>Phone</Label>
+                                <Input type="text" placeholder="+1 2345 6789" />
+                            </FormInput>
+                            <FormInput>
+                                <Label>Password</Label>
+                                <Input type="password" />
+                            </FormInput>
+                            <FormInput>
+                                <Label>Address</Label>
+                                <Input type="text" placeholder="Address" />
+                            </FormInput>
+                            <FormInput>
+                                <Label>Country</Label>
+                                <Input type="text" placeholder="USA" />
+                            </FormInput>
+                        </Form>
+                    </Right>
                 </Bottom>
             </NewContainer>
         </Container>
@@ -32,20 +65,62 @@ const NewContainer = styled.div`
 `;
 
 const Top = styled.div`
-  
+    -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+    box-shadow: 2px 4px 10px 1px rgba(201, 201, 201, 0.47);
+    padding: 10px;
+    margin: 20px;
+    display: flex;
+`;
+
+const Title = styled.h1`
+    color: lightgray;
+    font-size: 20px;
 `;
 
 const Bottom = styled.div`
-  
+    -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+    box-shadow: 2px 4px 10px 1px rgba(201, 201, 201, 0.47);
+    padding: 10px;
+    margin: 20px;
+    display: flex;
 `;
 
 const Left = styled.div`
-  
+    flex: 1;
+    text-align: center;
+`;
+
+const Image = styled.img`
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    object-fit: cover;
 `;
 
 const Right = styled.div`
+  flex: 2;
+`;
+
+const Form = styled.form`
   
 `;
+
+const FormInput = styled.div`
+  
+`;
+
+const Input = styled.input`
+  
+`;
+
+const Label = styled.label`
+  
+`;
+
+const Icon = styled.div`
+  
+`;
+
 
 
 
