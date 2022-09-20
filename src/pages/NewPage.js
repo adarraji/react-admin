41088458eba6b1881314c3d7c3810b3e4e2a31f2
Ali Darraji <techbrain203@outlook.com>
2022-09-19 20:28:04 -0400

    added sections

--- src/pages/NewPage.js
+++ src/pages/NewPage.js
@@ -1,9 +1,53 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+
 
 const NewPage = () => {
     return (
-        <div>NewPage</div>
+        <Container>
+            <Sidebar />
+            <NewContainer>
+                <Navbar />
+                <Top>
+
+                </Top>
+                <Bottom>
+                    <Left></Left>
+                    <Right></Right>
+                </Bottom>
+            </NewContainer>
+        </Container>
     )
 }
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const NewContainer = styled.div`
+  flex: 6;
+`;
+
+const Top = styled.div`
+  
+`;
+
+const Bottom = styled.div`
+  
+`;
+
+const Left = styled.div`
+  
+`;
+
+const Right = styled.div`
+  
+`;
+
+
+
 
 export default NewPage
